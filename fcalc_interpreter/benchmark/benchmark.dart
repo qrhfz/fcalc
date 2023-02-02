475811e77eed4ab629f8b992794dda1c3775e7b3
Qori El-Hafizh <contact@qori.dev>
2023-02-02 23:58:53 +0700

    Update benchmark.dart

--- fcalc_interpreter/benchmark/benchmark.dart
+++ fcalc_interpreter/benchmark/benchmark.dart
@@ -1,5 +1,5 @@
 import 'package:benchmark_harness/benchmark_harness.dart';
-import 'package:fcalc_interpreter/src/env.dart';
+import 'package:fcalc_interpreter/fcalc_interpreter.dart';
 
 class InterpreterBenchmark extends BenchmarkBase {
   const InterpreterBenchmark() : super('Template');
@@ -10,9 +10,9 @@ class InterpreterBenchmark extends BenchmarkBase {
 
   @override
   void run() {
-    final env = Env.global();
-    env.run("f(x)=x^4-3×x^3+2×x^2-x+5");
-    env.run('f(7)');
+    final interpreter = Interpreter();
+    interpreter.run("f(x)=x^4-3×x^3+2×x^2-x+5");
+    interpreter.run('f(7)');
   }
 
   @override
